e1b8f4bdcbdbc00c6bd479b39d4d6a91967851be
EvgenyKashin <kashinevge@gmail.com>
2016-09-18 13:35:55 +0300

    Downloading posts added

--- scraper.py
+++ scraper.py
@@ -1,8 +1,14 @@
 import requests
 import json
+import math
+import time
+import pickle
 import config
 
 posts_url = 'https://api.vk.com/method/wall.get?owner_id={}&\
+           &access_token={}&v=5.52&filter=owner&count={}&offset={}'
+
+posts_url_domain = 'https://api.vk.com/method/wall.get?domain={}&\
            &access_token={}&v=5.52&filter=owner&count={}&offset={}'
 
 
@@ -15,9 +21,43 @@ def get_posts(owner_id, count=5, offset=0):
 
 def post_condition(post):
     return post['post_type'] == 'post' and\
-           len(post['text']) > config.MIN_CHARACTERS
+           len(post['text']) > config.MIN_CHARACTERS and\
+           'http:' not in post['text'] and\
+           len([att for att in post['attachments']
+                if att['type'] == 'video']) == 0
 
 
 def parse_posts(posts):
     posts = list(filter(post_condition, posts['response']['items']))
-    return posts
+    return [{'text': p['text'], 'likes': p['likes']['count'],
+             'reposts':p['reposts']['count']} for p in posts]
+
+
+def downloads_posts(ownder_id, max_iter=None):
+    result_posts = []
+    post = get_posts(ownder_id, 1)
+    count = int(post['response']['count'])
+    print(count, 'total posts')
+    iteration = math.ceil(count / 100)
+    filename = 'data/{}.txt'.format(ownder_id)
+    if max_iter:
+        iteration = min(max_iter, iteration)
+
+    for i in range(iteration):
+        if i % 10 == 0:
+            print('{:.2f}%'.format(i / iteration * 100))
+        posts = get_posts(ownder_id, 100, i * 100)
+        posts = parse_posts(posts)
+        result_posts.extend(posts)
+
+        """
+        with open(filename, 'a', encoding='utf-8') as f:
+            f.write('\n\n'.join(posts))
+            f.write('\n\n')
+        """
+        time.sleep(0.3)
+
+    with open(filename, 'wb') as f:
+        pickle.dump(result_posts, f)
+    print(len(result_posts), 'Posts downloaded')
+    return result_posts
